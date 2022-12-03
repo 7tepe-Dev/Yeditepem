4c7bb664148a421ad7891e9ac379f046e868ad3b
muslumakgul <muslum280@gmail.com>
2022-12-03 13:23:38 +0300

    Eksikler var.

--- app/src/main/java/com/example/yeditepem/WelcomePage.kt
+++ app/src/main/java/com/example/yeditepem/WelcomePage.kt
@@ -2,10 +2,18 @@ package com.example.yeditepem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 
 class WelcomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_page)
+
+
+        window.decorView.systemUiVisibility = View.SYSTEM.UI_FLAG_FULLSCREEN
+        supportActionBar?.hide()
     }
+
+
 }
