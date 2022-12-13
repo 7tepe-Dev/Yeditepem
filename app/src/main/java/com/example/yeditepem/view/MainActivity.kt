48266b80ca791e0ea2ced61843fcff5bef2e1cd9
Yusuf Kağan Özetci <yusufkaganozetci@hotmail.com>
2022-12-13 18:26:33 +0300

    Runnable and Handler

--- app/src/main/java/com/example/yeditepem/view/MainActivity.kt
+++ app/src/main/java/com/example/yeditepem/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.yeditepem.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.widget.TextView
 import androidx.lifecycle.Observer
@@ -11,12 +12,19 @@ import com.example.yeditepem.viewmodel.StudentViewModel
 
 class MainActivity : AppCompatActivity() {
 
-
+    var runnable:Runnable = Runnable{}
+    var handler = Handler()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        handler = Handler()
+        var y = findViewById<TextView>(R.id.test) as TextView
+        //val tempstatus = MutableLiveData<String>()
 
-        var y = findViewById<TextView>(R.id.dbButton) as TextView
+        // The external immutable LiveData for the request status
+
+
+
 
         var studentViewModel = StudentViewModel()
         studentViewModel.getStudents()
@@ -24,8 +32,24 @@ class MainActivity : AppCompatActivity() {
         studentViewModel.studentList.observe(this, Observer{
             if (studentViewModel.studentList.value != null){
                 y.text = studentViewModel.studentList.value!![0].toString()
+
+                //println(studentViewModel.studentList.value)
             }
         })
+
+        runnable = Runnable {
+            handler.postDelayed(runnable,10)
+            if(studentViewModel.studentList.value == null){
+                println("Hala Null")
+            }
+            else{
+                println("Artık null degil")
+                println(studentViewModel.studentList.value!![0].id)
+                handler.removeCallbacks(runnable)
+            }
+        }
+        handler.post(runnable)
+        //handler.removeCallbacks(runnable)
 
     }
 
