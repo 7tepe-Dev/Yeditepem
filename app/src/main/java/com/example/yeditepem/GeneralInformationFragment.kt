fd2d5164a8f0608e4b7169a7a6de7c925d92d6ad
Yusuf Kağan Özetci <yusufkaganozetci@hotmail.com>
2023-01-03 13:24:46 +0300

    Dynamic

--- app/src/main/java/com/example/yeditepem/GeneralInformationFragment.kt
+++ app/src/main/java/com/example/yeditepem/GeneralInformationFragment.kt
@@ -5,55 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [GeneralInformationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GeneralInformationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_general_information, container, false)
+        val layout = inflater.inflate(R.layout.fragment_general_information, container, false)
+        //createViewsTemp(layout)
+        return layout
     }
+/*Dinamik deneme
+    fun createViewsTemp(layout:View){
+        val desiredLayout = layout.findViewById<LinearLayout>(R.id.myLayout)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GeneralInformationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GeneralInformationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+        repeat(50){
+            var createdThing = LayoutInflater.from(layout.context).inflate(R.layout.activity_general_information_list_view, null,false)
+
+            desiredLayout.addView(createdThing)
+        }
     }
+*/
 }
---
+++ app/src/main/java/com/example/yeditepem/listviews/GeneralInformationListView.kt
@@ -0,0 +1,12 @@
+package com.example.yeditepem.listviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.yeditepem.R
+
+class GeneralInformationListView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_general_information_list_view)
+    }
+}
